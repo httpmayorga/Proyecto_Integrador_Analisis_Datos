9cac511190e20f421943551117f71b6cc274ed84
Andres J. Mayorga <53920424+httpmayorga@users.noreply.github.com>
2023-11-23 11:55:27 -0500

    Add files via upload

---
+++ Parte1.py
@@ -0,0 +1,12 @@
+import numpy as np
+from datasets import load_dataset
+
+dataset = load_dataset("mstz/heart_failure")
+data = dataset["train"]
+edades = data["age"]
+
+edades_np = np.array(edades)
+promedio_edad = np.mean(edades_np)
+
+print(f"El promedio de edad de las personas participantes es: {promedio_edad:.2f} años")
+
